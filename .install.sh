24ccdaf1f7c0c8dd95f724b2d8a4c2b4f95cbb62
Alejandro Escontrela <alejandroescontrela@gmail.com>
2024-10-10 17:13:09 -0700

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -131,14 +131,19 @@ brew install --cask font-fira-code
 
 # macOS Settings
 # echo "Changing macOS defaults..."
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock "mru-spaces" -bool "false"
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true
+defaults write com.apple.finder DisableAllAnimations -bool true
+defaults write com.apple.Finder AppleShowAllFiles -bool true
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Additional settings...
 # defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 # defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 # defaults write com.apple.spaces spans-displays -bool false
-# defaults write com.apple.dock autohide -bool true
-# defaults write com.apple.dock "mru-spaces" -bool "false"
-# defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 # defaults write com.apple.LaunchServices LSQuarantine -bool false
-# defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 # defaults write NSGlobalDomain KeyRepeat -int 1
 # defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 # defaults write NSGlobalDomain AppleShowAllExtensions -bool true
@@ -148,13 +153,10 @@ brew install --cask font-fira-code
 # defaults write com.apple.screencapture location -string "$HOME/Desktop"
 # defaults write com.apple.screencapture disable-shadow -bool true
 # defaults write com.apple.screencapture type -string "png"
-# defaults write com.apple.finder DisableAllAnimations -bool true
 # defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
 # defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 # defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
 # defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
-# defaults write com.apple.Finder AppleShowAllFiles -bool true
-# defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 # defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 # defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 # defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
