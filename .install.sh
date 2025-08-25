e1b181abf7b237c28c69bcb97ed8625227743aa5
Alejandro Escontrela <alejandroescontrela@gmail.com>
2025-08-25 10:45:31 -0700

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -81,27 +81,27 @@ defaults write com.apple.dock expose-group-apps -bool true && killall Dock
 # Additional settings...
 # defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 # defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-# defaults write com.apple.spaces spans-displays -bool false
+defaults write com.apple.spaces spans-displays -bool false
 # defaults write com.apple.LaunchServices LSQuarantine -bool false
 # defaults write NSGlobalDomain KeyRepeat -int 1
 # defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-# defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 # defaults write NSGlobalDomain _HIHideMenuBar -bool true
 # defaults write NSGlobalDomain AppleHighlightColor -string "0.65098 0.85490 0.58431"
 # defaults write NSGlobalDomain AppleAccentColor -int 1
-# defaults write com.apple.screencapture location -string "$HOME/Desktop"
+defaults write com.apple.screencapture location -string "$HOME/Desktop"
 # defaults write com.apple.screencapture disable-shadow -bool true
-# defaults write com.apple.screencapture type -string "png"
+defaults write com.apple.screencapture type -string "png"
 # defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
 # defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 # defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
 # defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
-# defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-# defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-# defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 # defaults write com.apple.finder ShowStatusBar -bool false
 # defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool YES
-# defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
 # defaults write com.apple.Safari IncludeDevelopMenu -bool true
 # defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 # defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
